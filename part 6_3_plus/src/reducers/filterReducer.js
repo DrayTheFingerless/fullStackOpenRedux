0111257c0072cd1cb086c21205c64d24a1545814
Rob <robert.ferreiraw90@gmail.com>
2025-01-31 14:41:40 +0000

    part 6, exercise 10

--- part 6_3_plus/src/reducers/filterReducer.js
+++ part 6_3_plus/src/reducers/filterReducer.js
@@ -1,18 +1,17 @@
 
-export const filterAnecdotes = (filter) => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter
-  }
-}
+import { createSlice } from "@reduxjs/toolkit"
 
-const filterReducer = (state = '', action) => {
-    switch (action.type) {
-      case 'SET_FILTER':
-        return action.payload
-      default:
-        return state
-    }
-  }
+var initialState = ''
 
-export default filterReducer
+const filterSlice = createSlice({  
+  name: 'notes',
+  initialState,
+      reducers: {
+        filterAnecdotes(state, action) {
+          return action.payload
+        }
+      }
+})
+
+export const { filterAnecdotes } = filterSlice.actions
+export default filterSlice.reducer
